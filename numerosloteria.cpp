3bf1c6f0796dba6264f512f518f17f61ec1fde63
Mauricio G. Coello <hmgc_mauro@icloud.com>
2019-01-26 21:16:29 -0600

    hw2

--- numerosloteria.cpp
+++ numerosloteria.cpp
@@ -7,6 +7,7 @@
 */
 #include <iostream>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h> 
 #include <cmath>
 #include <string>
@@ -23,14 +24,14 @@ int main()
 	int isAround = 0;
 	int random = 0;
 	int rounds = 100000;
+	long int rounds1 = 999999999;
 	srand (time(NULL));
-	int commonCoordinateNum = 0;
-	string commonCoordinateSt;
 	map<int, int> seventy;
 	map<int, int> twentyfive;
 	pair<std::map<int,int>::iterator,bool> ret;
 	vector<pair<int,int>>vec;
 	vector<pair<int,int>>vec1;
+	cout<<"________MUST COMMON PAIR________"<<endl;
 	for(int i = 0; i <rounds; i++)
 	{
 		random = rand() % 70+1;
@@ -57,6 +58,82 @@ int main()
 	}
 	cout<<"____1 to 25____"<<endl;
 	cout<<"NUM: "<<vec1[0].first<<":"<<vec1[0].second<<endl;
+	map<int, int> seventyGlobal;
+	map<int, int> twentyfiveGlobal;
+	map<int, int> seventy1;
+	map<int, int> twentyfive1;
+	map<int,int>::iterator it;
+	pair<std::map<int,int>::iterator,bool> ret1;
+	bool firstmatch = false;
+	bool secondmatch = false;
+	for(int i = 1; i <= rounds1; i++)
+	{
+		twentyfive1 = twentyfiveGlobal;
+		seventy1 = seventyGlobal;
+		if(i==1)
+		{
+			cout<<"____CHECKING WITH NUMS:____"<<endl;
+			for(int j = 0; j < 5; j++)
+			{
+				random = rand() % 70+1;
+				ret = seventy1.insert ( std::pair<int,int>(random,1));
+				if (ret.second==false) 
+				{
+					seventy1[random]++;
+				}
+				if(i==4)
+				{
+					cout<<random;
+				}
+				else
+				{
+					cout<<random<<", ";
+				}
+			}
+			cout<<random<<" & ";
+			random = rand() % 25+1;
+			ret1 = twentyfive1.insert ( std::pair<int,int>(random,1));
+			cout<<random<<endl;
+			seventyGlobal = seventy1;
+			twentyfiveGlobal = twentyfive1;			
+		}
+		for(int j = 0; j < 5; j++)
+		{
+			random = rand() % 70+1;
+			it = seventy1.find(random);
+			if(it!=seventy1.end())
+			{
+				if(seventy1[random]>1)
+				{
+					seventy1[random]--;
+				}
+				else
+				{
+					it = seventy1.find(random);
+					seventy1.erase(it);
+				}
+			}
+		}
+		random = rand() % 25+1;
+		it = twentyfive1.find(random);
+		if(it!=twentyfive1.end())
+		{
+			if(twentyfive1[random]>1)
+			{
+				twentyfive1[random]--;
+			}
+			else
+			{
+				it = twentyfive1.find(random);
+				twentyfive1.erase(it);
+			}
+		}
+		if(seventy1.empty() && twentyfive1.empty())
+		{
+			cout<<"--FOUND MATCH AFTER "<<i<<" ITERATIONS!"<<endl;
+			i = rounds1+1;
+		}
+	}
 	return 0;
 
 }
